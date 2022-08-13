d7a9d7459927e17933cb8425158d5c5bfebdd6f4
khizerdev <46632721+khizerdev@users.noreply.github.com>
2022-08-13 14:25:10 +0500

    Fetching data

--- pages/todos/index.vue
+++ pages/todos/index.vue
@@ -3,13 +3,23 @@
         Todos
         <nuxt-link :to="{ name: 'todos-create'}">Create todos</nuxt-link>
         <nuxt-link :to="{ name: 'todos-todo-id' , params : { id: 5}}">Specific Todo</nuxt-link>
+
+  
+
+        <ul>
+            <li v-for="item in todos">
+                {{item.title}} - {{item.id}}
+            </li>
+        </ul>
     </div>
 </template>
 
-<script>
-    export default {
-        
-    }
+<script setup>
+
+    // const {data} = await useAsyncData('items' , () => $fetch('https://jsonplaceholder.typicode.com/todos'));
+
+    const {data:todos} = useFetch('https://jsonplaceholder.typicode.com/todos')
+    
 </script>
 
 <style>
